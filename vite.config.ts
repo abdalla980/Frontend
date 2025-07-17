3649d165fd22dd849b877744f9f95f074ecbfee1
abdalla980 <100001276@stud.srh-university.de>
2025-07-17 02:02:29 +0200

    ready1

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 export default defineConfig({
   base: '/Frontend/',
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: path.resolve(__dirname, 'dist/index.html'),
-          dest: '', // copies to dist/404.html after build
-          rename: '404.html'
-        }
-      ]
-    })
+
   ]
 });
 
